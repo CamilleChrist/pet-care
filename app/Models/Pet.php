7d315cb7b00b90918ed9a9266b7c3dd50cd79232
CamilleChrist <christ.camille@orange.fr>
2026-09-09 14:35:04 +0200

    fix(pets): correct last_vet_visit_at in Pet fillable

--- app/Models/Pet.php
+++ app/Models/Pet.php
@@ -3,18 +3,21 @@
 namespace App\Models;
 
 use App\Enums\PetGender;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pet extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'gender',
         'birth_date',
         'name',
         'breed_id',
-        'last_pet_visit_at',
+        'last_vet_visit_at',
         'health_notes',
     ];
 
