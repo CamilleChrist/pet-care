0fa65bd521247610a90e03e80d2b04ac46b63358
CamilleChrist <christ.camille@orange.fr>
2026-09-09 14:39:14 +0200

    feat(pets): restrict pet access to their owner

---
+++ app/Policies/PetPolicy.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Pet;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PetPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Pet $pet): Response
+    {
+        return $user->id === $pet->user_id
+            ? Response::allow()
+            : Response::deny('Vous ne possedez pas cet animal.');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Pet $pet): Response
+    {
+        return $user->id === $pet->user_id
+            ? Response::allow()
+            : Response::deny('Vous ne possedez pas cet animal.');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Pet $pet): Response
+    {
+        return $user->id === $pet->user_id
+            ? Response::allow()
+            : Response::deny('Vous ne possedez pas cet animal.');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Pet $pet): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Pet $pet): bool
+    {
+        return false;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -43,12 +43,12 @@ Route::get('/dashboard', function () {
 })->name('dashboard')->middleware('auth');
 
 // ****  PETS  **** //
-Route::prefix('/pets')->controller(PetController::class)->group(function () {
+Route::prefix('/pets')->controller(PetController::class)->middleware('auth')->group(function () {
     Route::get('/', 'index')->name('pets.index');
     Route::get('/create', 'create')->name('pets.create');
     Route::post('/store', 'store')->name('pets.store');
-    Route::get('/{pet}', 'show')->name('pets.show');
-    Route::get('/{pet}/edit', 'edit')->name('pets.edit');
-    Route::match(['put', 'post'], '/{pet}/update', 'update')->name('pets.update');
-    Route::match(['delete', 'post'], '/{pet}/delete', 'destroy')->name('pets.destroy');
-})->middleware(['auth']);
+    Route::get('/{pet}', 'show')->name('pets.show')->can('view', 'pet');
+    Route::get('/{pet}/edit', 'edit')->name('pets.edit')->can('update', 'pet');
+    Route::match(['put', 'post'], '/{pet}/update', 'update')->name('pets.update')->can('update', 'pet');
+    Route::match(['delete', 'post'], '/{pet}/delete', 'destroy')->name('pets.destroy')->can('delete', 'pet');
+});
