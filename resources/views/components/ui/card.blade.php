2b9ffce90dc7ba156bdc1d0a8be9bc402289bd35
CamilleChrist <christ.camille@orange.fr>
2026-09-20 18:03:35 +0200

    feat(ui): make card title optional and add a subtitle

--- resources/views/components/ui/card.blade.php
+++ resources/views/components/ui/card.blade.php
@@ -1,17 +1,26 @@
 @props([
-    'tagTitle' => 'h2',
-    'title',
+    'tagTitle' => 'h3',
+    'subtitle' => null,
+    'title' => null,
     'description' => null,
 ])
 
 <div {{ $attributes->class(['card']) }}>
-    <header class="card__header">
-        <{{ $tagTitle }} class="card__title">{{ $title }}</{{ $tagTitle }}>
+    @if ($subtitle || $title || $description)
+        <header class="card__header">
+            @if ($subtitle)
+                <p class="card__subtitle">{{ $subtitle }}</p>
+            @endif
 
-        @if ($description)
-            <p class="card__description">{{ $description }}</p>
-        @endif
-    </header>
+            @if ($title)
+                <{{ $tagTitle }} class="card__title">{{ $title }}</{{ $tagTitle }}>
+            @endif
+
+            @if ($description)
+                <p class="card__description">{{ $description }}</p>
+            @endif
+        </header>
+    @endif
 
     {{ $slot }}
 </div>
