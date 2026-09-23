01478a78c20ff41d5a16589d5dd814b55d4cde1b
CamilleChrist <christ.camille@orange.fr>
2026-09-23 21:20:10 +0200

    fix(pets): move the delete button to the bottom of the pet page

--- resources/views/pets/edit.blade.php
+++ resources/views/pets/edit.blade.php
@@ -8,15 +8,4 @@
         'submitLabel' => 'Enregistrer',
     ])
 
-    <div class="form__actions">
-        <button type="button" class="btn btn--ghost-danger" data-dialog-open="delete-pet">
-            <x-ui.icon name="trash-2" class="btn__icon"/>
-            Supprimer la fiche
-        </button>
-    </div>
-
-    <x-ui.confirm-delete id="delete-pet" :action="route('pets.destroy', $pet)"
-                         title="Supprimer {{ $pet->name }} ?"
-                         description="La fiche, les pesées et les vaccins de {{ $pet->name }} seront définitivement supprimés." />
-
 </x-layouts.app>
